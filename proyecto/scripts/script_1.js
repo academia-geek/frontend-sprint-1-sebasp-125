a1d086bde4c87836d763169e9f6e9e4f2e29503f
sebasp-125 <sp6027193@gmail.com>
2023-12-14 21:06:23 -0600

    imagenes Agregadas

--- proyecto/scripts/script_1.js
+++ proyecto/scripts/script_1.js
@@ -5,6 +5,6 @@ const body_logo1 = document.getElementById("body_logo1")
 primer_logo.addEventListener('click' , function() {
     primer_logo.classList.remove("img_original");
     primer_logo.classList.add("clase_modificada");
-
+    
     //Cambio de imagen al hacer click en la primera logo
 })
