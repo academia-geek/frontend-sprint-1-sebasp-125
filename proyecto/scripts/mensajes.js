1ae3a7d4875aebc1d690441d77cb59ad6aa34601
sebasp-125 <sp6027193@gmail.com>
2023-12-25 20:46:34 -0600

    chat

--- proyecto/scripts/mensajes.js
+++ proyecto/scripts/mensajes.js
@@ -3,6 +3,11 @@ const home = document.getElementById("home")
 const mensajes = document.getElementById("mensaje") 
 const corazon = document.getElementById("corazon")
 const usuario = document.getElementById("usuario")
+const btn_mensaje = document.getElementById("btn_mensaje")
+
+btn_mensaje.addEventListener("click", function(){
+    window.location.href = "chat.html"
+})
 
 home.addEventListener("click" , function(){
     window.location.href = "Adopcion_Mascotas.html"
